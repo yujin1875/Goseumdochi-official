74d70e4514c7b2300258b5a69099b8f1588ff158
eeeong <keroro010112@gmail.com>
2024-02-28 20:14:14 +0900

    Project Setting #1

---
+++ src/main/java/com/midas/goseumdochi/GoseumdochiApplication.java
@@ -0,0 +1,13 @@
+package com.midas.goseumdochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoseumdochiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoseumdochiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midas/goseumdochi/GoseumdochiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midas.goseumdochi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoseumdochiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
