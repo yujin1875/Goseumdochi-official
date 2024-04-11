529f68b2cfcba35d254670fd65996df124e0a30f
조하은 <nancy323@naver.com>
2024-04-11 16:47:41 +0900

    #add 관리자 공지사항 목록 확인 및 작성 기능, 학원신청서 수락/거절 기능, 수락 시 id/pw 부여 기능

--- src/main/java/com/midas/goseumdochi/admin/controller/AdminMainController.java
+++ src/main/java/com/midas/goseumdochi/admin/controller/AdminMainController.java
@@ -1,8 +1,16 @@
 package com.midas.goseumdochi.admin.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class AdminMainController {
 
-// 필요 없을 듯
+    @Autowired
+    private HttpSession httpSession;
+
+    private boolean isLoggedIn() {
+        // 세션에서 관리자 ID를 가져와서 확인 ㄱㄱ
+        return httpSession.getAttribute("adminId") != null;
+    }
 }
