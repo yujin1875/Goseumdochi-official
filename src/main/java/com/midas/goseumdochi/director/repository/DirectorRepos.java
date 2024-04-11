9a8bc868356b9b8b7090991a8182f4a9663bfe7b
eeeong <keroro010112@gmail.com>
2024-04-11 22:33:17 +0900

    #update 학원신청서폼 양식추가/리액트 연동

--- src/main/java/com/midas/goseumdochi/director/repository/DirectorRepos.java
+++ src/main/java/com/midas/goseumdochi/director/repository/DirectorRepos.java
@@ -1,6 +1,5 @@
 package com.midas.goseumdochi.director.repository;
 
-import com.midas.goseumdochi.director.entity.AcademyFormEntity;
 import com.midas.goseumdochi.director.entity.DirectorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface DirectorRepository extends JpaRepository<DirectorEntity, Long> {
+public interface DirectorRepos extends JpaRepository<DirectorEntity, Long> {
     @Query("SELECT d FROM DirectorEntity d WHERE d.loginid = :loginid And d.password = :password")
     Optional<DirectorEntity> findByLoginidAndPassword(@Param("loginid") String loginid,
                                                                          @Param("password") String password);
