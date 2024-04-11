be68b0edeb465cccdfc983f0a6a66effab6d5513
eeeong <keroro010112@gmail.com>
2024-04-11 22:33:17 +0900

    #update 학원신청서폼 양식추가/리액트 연동

--- src/main/java/com/midas/goseumdochi/director/service/DirectorService.java
+++ src/main/java/com/midas/goseumdochi/director/service/DirectorService.java
@@ -2,7 +2,7 @@ package com.midas.goseumdochi.director.service;
 
 import com.midas.goseumdochi.director.dto.DirectorDTO;
 import com.midas.goseumdochi.director.entity.DirectorEntity;
-import com.midas.goseumdochi.director.repository.DirectorRepository;
+import com.midas.goseumdochi.director.repository.DirectorRepos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class DirectorService {
-    private final DirectorRepository directorRepository;
+    private final DirectorRepos directorRepository;
 
     // 로그인아이디와 비밀번호로 원장 찾기
     public DirectorDTO findDirector(String loginid, String password) { //* 리턴을 Optional로 할까
