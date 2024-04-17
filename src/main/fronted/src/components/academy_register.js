7113e9d5303827e81b10c4996a76572a5a0fbce8
hyoju <nicehyoju!@gmail.com>
2024-04-17 21:32:50 +0900

    #add academy_register 연동

--- src/main/fronted/src/components/academy_register.js
+++ src/main/fronted/src/components/academy_register.js
@@ -2,12 +2,10 @@ import '../css/academy_register.css';
 import logo from './images/goseumdochi.png';
 import React, { useState } from 'react';
 import axios from 'axios';
-//* 에러나누
-import { useNavigate } from 'react-router-dom'; // useHistory 훅 말고 useNavigate!! 임포트합니다.
+import { useNavigate } from 'react-router-dom';
 import Post from "../components/util/post";
 
 function App7() {
-    // 입력받은 변수 저장 (Controller로 보낼거임)
     const [inputs, setInputs] = useState({
         academyName: '',
         academyPhoneNumber: '',
@@ -18,16 +16,14 @@ function App7() {
         directorPhoneNumber: '',
         authNumber: ''
     });
-    // 페이지 변경 위해
+
     const navigate = useNavigate();
 
-    // 우편주소 찾기 팝업 띄우기 변수
     const [popup, setPopup] = useState(false);
     const handleComplete = (data) => {
         setPopup(!popup);
     }
 
-    // 입력이 바뀔때마다 inputs 변수값 변경
     const handleChange = (e) => {
         const { name, value } = e.target;
         setInputs(prevInputs => ({
@@ -36,20 +32,17 @@ function App7() {
         }));
     };
 
-    // 폼 제출하면 실행 함수
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            // axios를 사용하여 서버에 신청서 등록 요청을 보냅니다.
             const response = await
                 axios.post('/api/academy/form', inputs);
             console.log('Form Submit success:', response.data);
-            // 성공 후, 다른 페이지로 리디렉션합니다.
             alert("학원신청서 등록 성공")
             navigate('/');
         } catch (error) {
             console.error('Form Submit error:', error);
-            alert(`${error.response.data}`); // 작은따옴표(')가 아니라 왼쪽위(`)
+            alert(`${error.response.data}`);
         }
     };
 
