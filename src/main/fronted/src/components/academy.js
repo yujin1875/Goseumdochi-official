24c1f6ef9d541691667cda20c81da560b59213bf
hyoju <nicehyoju!@gmail.com>
2024-04-26 19:03:04 +0900

    #update academy_register

--- src/main/fronted/src/components/academy.js
+++ src/main/fronted/src/components/academy.js
@@ -26,7 +26,7 @@ function App4() {
                 <div id='academy_write_buttons'>
                     <a>본인 확인을 위한 신청자 이름과 전화번호를 입력해주세요</a>
                     <input type="name" placeholder="이름" value={inputDirectorName} onChange={handleInputChange1}/><hr/>
-                    <input type="phonenumber" placeholder="전화번호" value={inputsDirectorPhoneNumber} onChange={handleInputChange2}/><hr/>
+                    <input type="phonenumber" placeholder="010-0000-0000" value={inputsDirectorPhoneNumber} onChange={handleInputChange2}/><hr/>
                     <div id="btn_academy">
                         <input type="submit" value="신청서 제출" id="submit_btn" onClick={GoAcademyRegister}/>
                         <input type="submit" value="제출한 신청서 확인" id="check_btn"/>
---
+++ src/main/java/com/midas/goseumdochi/teacher/controller/TeacherController.java
@@ -0,0 +1,19 @@
+package com.midas.goseumdochi.teacher.controller;
+
+import com.midas.goseumdochi.teacher.dto.TeacherDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/teacher")
+public class TeacherController {
+
+    /*
+    @PostMapping("/regist")
+    public ResponseEntity<?> registTeacher(@RequestBody TeacherDTO inputTeacherDTO) {
+
+    }*/
+}
