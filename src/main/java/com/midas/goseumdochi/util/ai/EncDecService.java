18a0ce4eef8373bc396bb5bec13f61f79c4567a5
eeeong <keroro010112@gmail.com>
2024-06-02 15:38:13 +0900

    #Edit 암호복호, 비방글 api 연결 수정

--- src/main/java/com/midas/goseumdochi/util/ai/EncDecService.java
+++ src/main/java/com/midas/goseumdochi/util/ai/EncDecService.java
@@ -12,7 +12,7 @@ public class EncDecService {
     // AES 암호화
 //    @Transactional
     public String encrypt(String plaintext) {
-        String url = "http://34.64.149.50:5000/encrypt"; // Flask 서버 주소 + route 주소
+        String url = "http://34.64.213.105:5000/encrypt"; // Flask 서버 주소 + route 주소
         String password = "12345678"; // decrypt의 password와 꼭 같아야함!
 
         // Restful (Flask 서버)와 통신할 객체
@@ -23,7 +23,7 @@ public class EncDecService {
         headers.setContentType(MediaType.APPLICATION_JSON); //헤더의 Content-Type을 json으로 설정
 
         // input (JSON 형태)
-        String param = String.format("{\"plaintext\" : \"%s\", \"password\" : \"%s\"}", plaintext, password);
+        String param = String.format("{\"text\" : \"%s\", \"password\" : \"%s\"}", plaintext, password);
 
         // 변환된 JSON 문자열과 헤더 정보를 포함하는 HttpEntity 객체를 생성
         HttpEntity<String> httpEntity = new HttpEntity<String>(param, headers);
@@ -35,7 +35,7 @@ public class EncDecService {
 
     // AES 복호화
     public String decrypt(String encryptText) {
-        String url = "http://34.64.149.50:5000/decrypt";
+        String url = "http://34.64.213.105:5000/decrypt";
         String password = "12345678";
 
         RestTemplate restTemplate = new RestTemplate();
@@ -43,7 +43,7 @@ public class EncDecService {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        String param = String.format("{\"encrypted_text\" : \"%s\", \"password\" : \"%s\"}", encryptText, password);
+        String param = String.format("{\"text\" : \"%s\", \"password\" : \"%s\"}", encryptText, password);
 
         HttpEntity<String> httpEntity = new HttpEntity<String>(param, headers);
 
