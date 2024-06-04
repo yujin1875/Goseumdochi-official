9545bdeb36eee5bf065c75df8926ea8a465cd08a
조하은 <nancy323@naver.com>
2024-06-04 22:51:07 +0900

    #edit 관리자메인 수정중

--- src/main/fronted/src/components/adminmain.js
+++ src/main/fronted/src/components/adminmain.js
@@ -1,26 +1,19 @@
-import React, { useEffect, useState } from 'react';
 import '../css/adminmain.css';
 import logo from './images/goseumdochi.png';
 
 function App18() {
-    const [adminName, setAdminName] = useState(""); // 관리자 이름 상태 변수
-
-    useEffect(() => {
-        // 백엔드에서 관리자 이름 가져오기
-        fetch("/api/admin/main/info")
-            .then(response => response.text())
-            .then(data => setAdminName(data))
-            .catch(error => console.error('Error fetching admin info:', error));
-    }, []); // 컴포넌트가 마운트될 때 한 번만 실행
+    const GoAdminAcademyFormManage=()=>{
+        window.location.href='/admin/academy/form/manage'
+    }
 
     return (
         <div id="App">
             <div id="header_adminmain">
-                <img src={logo} alt="Logo"/>
+                <img src={logo}/>
             </div>
             <div id="admin_info">
                 <div id="info">
-                    <a>관리자: {adminName} </a>
+                    <a>관리자 : ㅇㅇㅇ 님</a>
                     <div id="logout">
                         로그아웃
                     </div>
@@ -30,7 +23,7 @@ function App18() {
                 <ul>
                     <li><a>전체 공지사항</a></li>
                     <li><a>학원 신청서</a></li>
-                    <li><a>학원 관리</a></li>
+                    <li><button onClick={GoAdminAcademyFormManage}>학원 관리</button></li>
                     <li><a>학생 관리</a></li>
                     <li><a>커뮤니티</a></li>
                 </ul>
