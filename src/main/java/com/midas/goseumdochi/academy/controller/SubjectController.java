6344f6e5bddbfb5298982e7168e3add7e96900f7
eeeong <keroro010112@gmail.com>
2024-07-25 20:37:16 +0900

    #Add 강의 (수정, 삭제)

--- src/main/java/com/midas/goseumdochi/academy/controller/SubjectController.java
+++ src/main/java/com/midas/goseumdochi/academy/controller/SubjectController.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class SubjectController {
     private final SubjectService subjectService;
 
+    // 과목 등록
     @PostMapping("/regist/academy/{academyId}")
     public ResponseEntity<?> registSubject(@PathVariable Long academyId, @RequestParam String inputSubjectName) { // (이름, 학원 id) 전달
         SubjectDTO findSubjectDTO = subjectService.findByNameAndAcademyId(inputSubjectName, academyId);
@@ -30,10 +31,39 @@ public class SubjectController {
         return ResponseEntity.ok(registSubjectDTO);
     }
 
+    // 학원에 등록된 과목 리스트 찾기
     @GetMapping("/findList/academy/{academyId}")
     public ResponseEntity<?> findSubjectList(@PathVariable Long academyId) {
         List<SubjectDTO> subjectDTOList = subjectService.findAllByAcademyId(academyId);
 
         return ResponseEntity.ok(subjectDTOList);
+    }
+
+    // 과목 수정
+    @PostMapping("/update/subject/{subjectId}")
+    public ResponseEntity<?> updateSubject(@PathVariable Long subjectId, @RequestParam String inputSubjectName) { // (이름, 학원 id) 전달
+        SubjectDTO subjectDTO = subjectService.update(subjectId, inputSubjectName);
+
+        if(subjectDTO != null) { // 과목 수정 실패
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED) // 에러 상태
+                    .body("과목 수정 실패.");
+        }
+
+        // 과목 등록 성공
+        return ResponseEntity.ok(subjectDTO);
+    }
+
+    // 과목 삭제
+    @PostMapping("/delete/subject/{subjectId}")
+    public ResponseEntity<?> deleteSubject(@PathVariable Long subjectId) {
+        if(subjectService.delete(subjectId) == false) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED) // 에러 상태
+                    .body("과목 삭제 실패.");
+        }
+
+        // 삭제 성공
+        return ResponseEntity.ok("과목 삭제 성공");
     }
 }
