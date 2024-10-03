bf0fa46b080c568a42ba1d34cd72756ea5ec56bc
eeeong <keroro010112@gmail.com>
2024-10-04 00:29:25 +0900

    #Add Ai api 연결(문서요약, 수학문제풀이)

---
+++ src/main/java/com/midas/goseumdochi/util/ai/MathSolveDTO.java
@@ -0,0 +1,13 @@
+package com.midas.goseumdochi.util.ai;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MathSolveDTO {
+    private String solve;
+    private String error;
+}
---
+++ src/main/java/com/midas/goseumdochi/util/ai/TextSummaryDTO.java
@@ -0,0 +1,12 @@
+package com.midas.goseumdochi.util.ai;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class TextSummaryDTO {
+    private String summary; // 요약된 문장
+}
