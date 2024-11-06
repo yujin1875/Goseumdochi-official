287e2fd263f9c6f534e067a2e59e07fcc9d195e4
Yang Yu Jin <kbg0154@yu.ac.kr>
2024-11-06 22:57:53 +0900

    #add 학생이 시험 시작해서 문제보기

---
+++ src/main/java/com/midas/goseumdochi/student/Service/StudentAnswerService.java
@@ -0,0 +1,45 @@
+package com.midas.goseumdochi.student.Service;
+
+import com.midas.goseumdochi.student.Dto.StudentAnswerDTO;
+import com.midas.goseumdochi.student.entity.StudentAnswerEntity;
+import com.midas.goseumdochi.student.Repository.StudentAnswerRepository;
+import com.midas.goseumdochi.teacher.entity.ExamQuestionEntity;
+import com.midas.goseumdochi.teacher.repository.ExamQuestionRepository;
+import com.midas.goseumdochi.teacher.repository.ExamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class StudentAnswerService {
+    private final StudentAnswerRepository studentAnswerRepository;
+    private final ExamQuestionRepository examQuestionRepository;
+
+    public void saveStudentAnswer(StudentAnswerDTO studentAnswerDTO) {
+
+        ExamQuestionEntity question = examQuestionRepository.findByIdAndExamEntityId(studentAnswerDTO.getQuestionId(), studentAnswerDTO.getExamId())
+                .orElseThrow(() -> new RuntimeException("해당 문제를 찾을 수 없습니다."));
+
+        StudentAnswerEntity answerEntity = new StudentAnswerEntity();
+        answerEntity.setStudentId(studentAnswerDTO.getStudentId());
+        answerEntity.setQuestion(question);
+        answerEntity.setAnswer(studentAnswerDTO.getAnswer());
+
+        studentAnswerRepository.save(answerEntity);
+    }
+
+    public List<StudentAnswerDTO> getStudentAnswers(Long studentId, Long examId) {
+        return studentAnswerRepository.findByStudentIdAndQuestionExamEntityId(studentId, examId).stream()
+                .map(entity -> new StudentAnswerDTO(
+                        entity.getId(),
+                        entity.getQuestion().getId(), // questionId 설정
+                        entity.getStudentId(),
+                        entity.getAnswer(),
+                        entity.getQuestion().getExamEntity().getId() // examId 설정
+                ))
+                .collect(Collectors.toList());
+    }
+}
